d52cf8f5d163b0217a69a629d7151ac5c551d0e6
barak5582 <Barak5582@gmail.com>
2023-09-03 17:10:41 +0300

    fix

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
-from fastapi import FastAPI, Request, Form, HTTPException
+from fastapi import FastAPI, Request, HTTPException
 import logging
-
-from fastapi.responses import HTMLResponse, StreamingResponse, FileResponse
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from xml.etree import ElementTree as ET
-from utils import (get_children, get_teachers, get_school_analytics, download_csv,
+from utils import (app, get_children, get_teachers, get_school_analytics, download_csv,
                    handle_manager_view, get_unregistered_students, get_teachers_by_subjects)
-import csv
-import io
 
 
-app = FastAPI(debug=True)
 app.mount("/stylesheets", StaticFiles(directory="stylesheets"), name="static")
-
 templates = Jinja2Templates(directory="templates")
 
+# Consts
 USERS = ["Teacher", "Parent", "School Manager"]
 
 
 # Home page
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
+    """Render the home page."""
     return templates.TemplateResponse("home.html", {"request": request, "user_types": USERS})
 
 
 # initialized the first page for each userType
 @app.get("/user", response_class=HTMLResponse)
 async def user_first_page(request: Request, userType: str):
+    """Render first page for a given user type."""
     if userType == "Teacher":
         return templates.TemplateResponse("teacher_subject_page.html", {"request": request})
     elif userType == "Parent":
@@ -41,8 +38,11 @@ async def user_first_page(request: Request, userType: str):
 # Second page for teacher. Fetch only the students that taking their class.
 @app.get("/teacher-action", response_class=HTMLResponse)
 async def teacher_page(request: Request, subject: str = None, action: str = None):
+    """Handle teacher actions - viewing or downloading data."""
     logging.info(f"fetching students of {subject} class")
     children_data = await get_children()
+
+    # Filter children data based on subject
     if subject:
         children_data = [child for child in children_data if subject.lower() in child.get('hobbies')]
     if action == 'View':
@@ -57,11 +57,17 @@ async def teacher_page(request: Request, subject: str = None, action: str = None
 # Second page for parent. Fetch only the family members.
 @app.get("/parent-action", response_class=HTMLResponse)
 async def parent_page(request: Request, family: str = None, action: str = None):
-    logging.info(f"fetching all students with family name: {family}")
+    """Handle parent actions - viewing or downloading data."""
+
+    # Get children and teacher data
     children_data = await get_children()
     teacher_data = await get_teachers()
+
+    # Filter children data based on family name
     if family:
         children_data = [child for child in children_data if family.lower() == child.get("last_name").lower()]
+
+    # Get teachers for the given students and subjects
     teachers = get_teachers_by_subjects(children=children_data, teachers=teacher_data)
 
     if action == 'View':
@@ -78,12 +84,17 @@ async def parent_page(request: Request, family: str = None, action: str = None):
 
 # Second page for manager.
 @app.get("/manager-action", response_class=HTMLResponse)
-async def manager_page(request: Request, subject: str = None, action: str = None):
+async def manager_page(request: Request, action: str = None):
+    """Handle school manager actions - viewing or downloading data."""
+
+    # Get children and teacher data
     children_data = await get_children()
     teachers_data = await get_teachers()
+
+    # Get list of unregistered students
     unregistered_students = get_unregistered_students(children_data)
-    students_by_subjects, teachers_data, num_of_students = handle_manager_view(children=children_data,
-                                                                               teachers=teachers_data)
+    students_by_subjects, num_of_students = handle_manager_view(children=children_data,
+                                                                teachers=teachers_data)
 
     if action == 'View':
         school_analytics = get_school_analytics(children=children_data, teachers=teachers_data)
@@ -96,8 +107,8 @@ async def manager_page(request: Request, subject: str = None, action: str = None
 
     if action == 'Download':
         files_to_download = {"teachers": teachers_data,
-                                   "children": children_data,
-                                   "unregistered_students": unregistered_students}
+                             "children": children_data,
+                             "unregistered_students": unregistered_students}
         files_to_download.update(students_by_subjects)
         return download_csv(files=files_to_download)
     else:
--- utils.py
+++ utils.py
@@ -12,14 +12,22 @@ import pandas as pd
 import os
 
 
-app = FastAPI()
+app = FastAPI(debug=True)
 
+# consts
 USERNAME = 'shadif@nelson-si.com'
 PASSWORD = 'J79$Qjnw4&acpM2y'
 
 
 @app.get("/teachers")
 async def get_teachers():
+    """
+        Fetches teachers' data from a specific URL and parses the XML response to
+        form a list of dictionaries with each dictionary containing details of a teacher.
+
+        Returns:
+            list: A list of dictionaries or JSONResponse in case of error.
+        """
     url = 'https://apigw-pod1.dm-us.informaticacloud.com/t/alfh1gf7rvmitxjlvzgs29.com/GetTeachers'
     response = requests.post(url, auth=HTTPBasicAuth(USERNAME, PASSWORD))
 
@@ -58,6 +66,13 @@ async def get_teachers():
 
 @app.get("/children")
 async def get_children():
+    """
+       Fetches children's data from a specific URL and parses the XML response to
+       form a list of dictionaries with each dictionary containing details of a child.
+
+       Returns:
+           list: A list of dictionaries or JSONResponse in case of error.
+       """
     logging.info("http request to get all children started")
     url = 'https://apigw-pod1.dm-us.informaticacloud.com/t/alfh1gf7rvmitxjlvzgs29.com/GetChildren'
     response = requests.post(url, auth=HTTPBasicAuth(USERNAME, PASSWORD))
@@ -99,7 +114,8 @@ def get_dict_of_subjects(teachers: dict) -> dict:
     return subjects
 
 
-def get_teachers_by_subjects(children: list, teachers: dict):
+def get_teachers_by_subjects(children, teachers):
+    """ This method get the teachers for a given list of subjects"""
     dict_of_subjects = get_dict_of_subjects(teachers)
 
     for child in children:
@@ -120,9 +136,14 @@ def get_teachers_by_subjects(children: list, teachers: dict):
 
 
 def get_school_analytics(children, teachers):
-
+    """
+        Generates a bar chart visualization of classroom occupancy based on children's hobbies (subjects).
+            :param children: A list of dictionaries, each containing details of a child.
+            :param teachers: A list of dictionaries, each containing details of a teacher.
+        Returns:
+            str: A base64 encoded string of the generated plot image.
+        """
     class_occupancy = get_dict_of_subjects(teachers)
-    print(class_occupancy)
 
     # count how many students in each class
     for children in children:
@@ -154,7 +175,17 @@ def get_school_analytics(children, teachers):
     return img
 
 
-def handle_manager_view(children: dict = None, teachers: dict = None):
+def handle_manager_view(children, teachers):
+    """
+        Processes the given lists of children and teachers to generate a view tailored for the manager.
+            :param children: A dictionary containing details of each child, including their hobbies (subjects).
+            :param teachers: A dictionary containing details of each teacher, including the subject they teach.
+        Returns:
+            Tuple[Dict, Dict, int]:
+                - The first dictionary maps each subject to the list of students interested in that subject.
+                - The second dictionary contains the original list of teachers.
+                - The integer represents the total number of children.
+        """
     student_by_sub = get_dict_of_subjects(teachers)
 
     for subject in student_by_sub:
@@ -165,10 +196,11 @@ def handle_manager_view(children: dict = None, teachers: dict = None):
             if subject.lower() in child.get('hobbies'):
                 student_by_sub[subject].append(child)
 
-    return student_by_sub, teachers, len(children)
+    return student_by_sub, len(children)
 
 
-def get_unregistered_students(children: dict) -> list:
+def get_unregistered_students(children) -> list:
+    """ This method returns all the students without any hobbies/unregistered to any class."""
     unregistered_students = []
     for child in children:
         if not child.get("hobbies"):
@@ -176,7 +208,17 @@ def get_unregistered_students(children: dict) -> list:
     return unregistered_students
 
 
-def download_csv(files: dict):
+def download_csv(files: dict) -> FileResponse:
+    """
+    Generates an Excel file containing various data tables and initiates a file download.
+    Parameters:
+        :param files: A dictionary where each key is a sheet name and the value is a list of dictionaries
+                      representing rows in that sheet. Each dictionary in the list should have matching keys
+                      that represent column names.
+    Returns:
+        FileResponse: A FastAPI FileResponse object to initiate a file download.
+                      The Excel file is named 'school_insights.xlsx'.
+    """
     with pd.ExcelWriter('school_insights.xlsx', engine='openpyxl') as writer:
         # Loop through each data type to create a DataFrame
         for filetype, data in files.items():
